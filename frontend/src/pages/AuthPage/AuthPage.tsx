4af7b44ee6e71d5e30f8381ab2decf41defcb0e8
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-01-13 13:49:32 +0200

    Remove Routes from AuthPage

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { ROUTES } from '../../utils/constants/routes'
 import { CreateAccount } from '../../components/Layout/Button/ButtonAuth/CreateAccount'
 
 export const AuthPage = () => {
 	return (
-		<Routes>
-			<Route path={ROUTES.AUTH} />
-		</Routes>
-
-
+		<div>
+			<h1>AuthPage works!</h1>
+			<CreateAccount />
+		</div>
 	)
 }
